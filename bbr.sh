7c97d9ddbacfde8a54bb3b087a8d5acd87dcef09
Fred <juyfchn@users.noreply.github.com>
2017-10-31 11:22:01 +0800

    解决headers下载速度慢

--- bbr.sh
+++ bbr.sh
@@ -40,7 +40,7 @@ installbbr(){
 
 
 	#Download Kernel V4.10
-	wget -O headers-all.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.15/linux-headers-4.10.15-041015_4.10.15-041015.201705080411_all.deb
+	wget -O headers-all.deb http://vir.jufxf.kiwi/linux-headers-4.10.15-041015_4.10.15-041015.201705080411_all.deb
 	dpkg -i headers-all.deb
 
 	if [[ ${bit} == "i386" ]]; then
